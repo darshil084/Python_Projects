10521b62ae2c5e6934d70123dc837fd86440d424
darshil084 <160143075+darshil084@users.noreply.github.com>
2025-06-08 13:40:14 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+# todo: add your appi key here
+
+apikey = "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"
